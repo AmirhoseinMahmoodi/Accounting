1af0686811c0698201471bcd5b35445f249adbf0
unknown <aidinmahmoodi82@gmail.com>
2022-01-02 17:26:24 +0330

    v.002.5-users information included

--- HWFINAL_4006127026_AmirhoseinMahmoodi.c
+++ HWFINAL_4006127026_AmirhoseinMahmoodi.c
@@ -8,7 +8,6 @@
 void intro();
 int mainmenu();
 void user_choice(int user_decision);
-void access_limitation();
 void log_in();
 void sign_up();
 int Hold;
@@ -16,15 +15,14 @@ void Hold_func();
 
 void main()
 {
+    int Hold;
     int user_decision;
     intro();
     Hold_func();
     user_decision = mainmenu();
-    printf("%d",user_decision);
-//    user_choice(user_decision);
     if(user_decision == 1)
     {
-        void log_in();
+        log_in();
     }
     if (user_decision == 2)
     {
@@ -85,32 +83,119 @@ int mainmenu()
 
 void Hold_func()
 {
+    FILE *Holder;
+    Holder = fopen("Hold.txt", "r");
+    fscanf(Holder, "%d",&Hold);
     if(Hold == 1)
     {
         printf("\nDo not restart the program!!\nOtherwise the hold timer will reset");
-        sleep(300000);
+        Sleep(300000);
         Hold = 0;
     }
 }
 
 void sign_up()
 {
-    char username[40] , password[40] ;
-    FILE *password_opener , *username_opener;
-    puts("Please enter your username up to 40 characters");
-    scanf("%s",username);
-    puts("Well done.\nNow enter your password up to 40 characters");
-    scanf("%s",password);
-    username_opener = fopen("username.txt", "a");
-    password_opener = fopen("Password.txt", "a");
-    fputs(username,username_opener);
-    fputs(password,password_opener);
-    fclose(username_opener);
-    fclose(password_opener);
+    struct user{
+        char username[41] ;
+        char password[41] ;
+        char pass_check[41] ;
+        char Name[41] ;
+        char surname[41] ;
+        char NID[11] ;
+        char PhoneNumber[10] ;
+        char Email[41] ;
+        struct user *Link ;
+    } TempUser ;
+    int loopcounter1 ;
+    FILE *user_opener ;
+    printf("Please enter your username up to 40 characters:");
+    scanf("%40s",TempUser.username);
+    printf("please enter your name:\n");
+    scanf("%40s",TempUser.Name);
+    printf("surname?\n");
+    scanf("%40s",TempUser.surname);
+    printf("Please enter your national ID code:\n");
+    scanf("%10s",TempUser.NID);
+    printf("Please enter your phone number:\n09");
+    scanf("%9s",TempUser.PhoneNumber);
+    printf("Please enter your Email:\n");
+    scanf("%40s",TempUser.Email);
+    printf("Well done.\nNow enter your password up to 40 characters\nTry to set a complicated password using capital and small letters and special characters:");
+    scanf("%40s",TempUser.password);
+    printf("enter your password again:");
+    scanf("%40s",TempUser.pass_check);
+    for(loopcounter1 = 0; loopcounter1 <= 1 ; loopcounter1++)
+    {
+        if(strcmp(TempUser.password,TempUser.pass_check) != 0)
+        {
+            printf("Passwords didnt match\n Try again:");
+            loopcounter1-=1 ;
+            scanf("%40s",TempUser.pass_check);
+        }
+        else
+        {
+            break;
+        }
+    }
+    user_opener = fopen("Users.txt", "a");
+    fprintf(user_opener, "%s\n%s\n%s\n%s\n09%s\n%s\n%s\n",TempUser.username,TempUser.Name,TempUser.surname,TempUser.NID,TempUser.PhoneNumber,TempUser.Email,TempUser.password);
+    fclose(user_opener);
 
 }
 
 void log_in()
 {
-
+    char username[41] , password[41] , username_user[41] , password_user[41] ;
+    int loop_counter1 , loop_counter2 , wrong = 0 ;
+    FILE *password_opener , *username_opener , *Holder;
+    password_opener = fopen("password.txt", "r");
+    username_opener = fopen("username.txt", "r");
+    fgets(username,40,username_opener);
+    fgets(password,40,password_opener);
+    printf("Please enter your username:");
+    for(loop_counter1 = 0;loop_counter1 <= 1; loop_counter1++)
+    {
+        scanf("%40s",username_user);
+        if(strcmp(username_user,username) != 0)
+        {
+            printf("There is not anyone with this username\nTry again:");
+            loop_counter1-=1;
+        }
+        else
+        {
+            break;
+        }
+    }
+    printf("Please enter your password:");
+    for(loop_counter2 = 0;loop_counter2 <= 1; loop_counter2++)
+    {
+        scanf("%40s",password_user);
+        if(strcmp(password_user,password) != 0)
+        {
+            printf("Incorrect password\n");
+            if(wrong == 4)
+            {
+                Holder = fopen("Hold.txt", "w");
+                Hold = 1;
+                fprintf(Holder, "%d",Hold);
+                printf("you have entered wrong password 5 times so the program will be banned for 5 minutes\nATTENTION - Do not restart the program otherwise the timer will be reset.");
+                fclose(Holder);
+                Sleep(300000);
+                Hold = 0;
+                Holder = fopen("Hold.txt", "w");
+                fprintf(Holder, "%d",Hold);
+                fclose(Holder);
+                wrong = 0;
+            }
+            printf("\nTry again:");
+            loop_counter2-=1;
+            wrong+=1;
+        }
+        else
+        {
+            break;
+        }
+    }
+    printf("you have entered successfully");
 }
